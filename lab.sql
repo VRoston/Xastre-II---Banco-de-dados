9ef0c62abb14c098e71108ad027d6c3669867dba
MatheusZCAR <107090798+MatheusZCAR@users.noreply.github.com>
2022-09-11 21:55:26 -0300

    Limite Alterado
    1 - O limite de caracteres em ''materias'' estava com ''9'', o que gerava um erro. Bug arrumado.
    2 - Mudei os comandos para letras maiúsculas

--- lab.sql
+++ lab.sql
@@ -1,31 +1,31 @@
-create table aluno
+CREATE TABLE aluno
 (
     RA varchar(9),
     nome  varchar(100),
-    apelido varchar(100),
-    materias varchar(9)
+    apelido varchar(50),
+    materias varchar(100)
 );
 
-create table professor
+CREATE TABLE professor
 (
     RP varchar(9),
     nome varchar(100),
-    apelido varchar(100),
-    materias varchar(9)
+    apelido varchar(50),
+    materias varchar(100)
 );
 
-insert into aluno values('A20000626', 'Lucas Albrechet Lopes Ruman', 'Roça', 'PI: Desenvolvimento de Sistemas Web');
-insert into aluno values('A22014203', 'Matheus Zanon Caritá', 'Mathias', 'Teologia E Fenômeno Humano');
-insert into aluno values('A22013310', 'Jean Okabe Rezende Piton', 'Jean', 'Organização de Sistmas de Computaçao');
-insert into aluno values('A22007263', 'Giovani Bellini dos Santos', 'virgula', 'Calculo I');
-insert into aluno values('A22006737', 'Victor de Melo Roston', 'Roston', 'Robótica Computacional');
-insert into aluno values('A22899140', 'Guilherme Bernardini Roelli', 'Zaldef', 'Metodos de Produção Recurciva Xastririca');
-insert into aluno values('A22900229', 'Maiza Leticia Oliveira', 'Lê', 'Programação Xastreriana I');
+INSERT INTO aluno VALUES('A20000626', 'Lucas Albrechet Lopes Ruman', 'Roça', 'PI: Desenvolvimento de Sistemas Web');
+INSERT INTO aluno VALUES('A22014203', 'Matheus Zanon Caritá', 'Mathias', 'Teologia E Fenômeno Humano');
+INSERT INTO aluno VALUES('A22013310', 'Jean Okabe Rezende Piton', 'Jean', 'Organização de Sistmas de Computaçao');
+INSERT INTO aluno VALUES('A22007263', 'Giovani Bellini dos Santos', 'virgula', 'Calculo I');
+INSERT INTO aluno VALUES('A22006737', 'Victor de Melo Roston', 'Roston', 'Robótica Computacional');
+INSERT INTO aluno VALUES('A22899140', 'Guilherme Bernardini Roelli', 'Zaldef', 'Metodos de Produção Recurciva Xastririca');
+INSERT INTO aluno VALUES('A22900229', 'Maiza Leticia Oliveira', 'Lê', 'Programação Xastreriana I');
 
-insert into professor values('P00000001', 'Leandro Alonso Xastre', 'Xastre', 'PI: Desenvolvimento de Sistemas Web');
-insert into professor values('P00000001', 'Leandro Alonso Xastre', 'Xastre', 'Programação Xastreriana I');
-insert into professor values('P00000001', 'Leandro Alonso Xastre', 'Xastre', 'Metodos de Produção Recurciva Xastririca');
-insert into professor values('P00000002', 'Valdomiro Placido dos Santos', 'Miro', 'Calculo I');
-insert into professor values('P00000003', 'Alexa IA', 'Alexa', 'Teologia E Fenômeno Humano');
-insert into professor values('P66666666', 'Ricardo Pannain', 'Pannain', 'Organização de Sistmas de Computaçao');
-insert into professor values('P00000004', 'Cleiton Josias Silveira', 'Cleitin', 'Robótica Computacional');
+INSERT INTO professor VALUES('P00000001', 'Leandro Alonso Xastre', 'Xastre', 'PI: Desenvolvimento de Sistemas Web');
+INSERT INTO professor VALUES('P00000001', 'Leandro Alonso Xastre', 'Xastre', 'Programação Xastreriana I');
+INSERT INTO professor VALUES('P00000001', 'Leandro Alonso Xastre', 'Xastre', 'Metodos de Produção Recurciva Xastririca');
+INSERT INTO professor VALUES('P00000002', 'Valdomiro Placido dos Santos', 'Miro', 'Calculo I');
+INSERT INTO professor VALUES('P00000003', 'Alexa IA', 'Alexa', 'Teologia E Fenômeno Humano');
+INSERT INTO professor VALUES('P66666666', 'Ricardo Pannain', 'Pannain', 'Organização de Sistmas de Computaçao');
+INSERT INTO professor VALUES('P00000004', 'Cleiton Josias Silveira', 'Cleitin', 'Robótica Computacional');
